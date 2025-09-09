c233435a0f7aca0e6560cfeee9b9d148029cdaeb
ABDUL BASIT <abdulbasitafaq78@gmail.com>
2025-09-09 14:18:49 +0500

    MCQS

---
+++ script.js
@@ -0,0 +1,197 @@
+alert(`HTML,CSS MCQS 
+    Write only answer keys alphabates`)
+
+
+
+var MCQS1 = prompt(`What does HTML stand for______________.
+
+a) HyperText Machine Language
+b) HyperText Markup Language
+c) HighText Marking Language
+d) HyperText Making Language`
+)
+
+
+if (MCQS1 == `b`) {
+    alert(`your answer is correct ✅`)
+     document.writeln
+(`1) Correct`+ `<br>`)
+
+} else {
+    alert(`your answer is incorrect ❌`)
+     document.writeln
+(`1) Incorrect`+ `<br>`)
+}
+
+
+
+
+var MCQS2 = prompt(`Which HTML tag is used to define the structure of an HTML document, including the head and body sections______________.
+
+a) <html>    
+b) <document>
+c) <webpage>
+d) <structure>`
+
+)
+
+
+if (MCQS2 == `a`) {
+    alert(`your answer is correct ✅`)
+     document.writeln
+(`2) Correct`+ `<br>`)
+
+} else {
+    alert(`your answer is incorrect ❌`)
+     document.writeln
+(`2) Incorrect`+ `<br>`)
+}
+
+
+
+
+var MCQS3 = prompt(`Which HTML tag is used to create an unordered list______________.
+
+a) <ol>
+b) <li>
+c) <ul>
+d) <list>
+e) <unorderlist>`
+
+)
+
+
+if (MCQS3 == `c`) {
+    alert(`your answer is correct ✅`)
+     document.writeln
+(`3) Correct`+ `<br>`)
+
+} else {
+    alert(`your answer is incorrect ❌`)
+     document.writeln
+(`3) Correct`+ `<br>`)
+}
+
+
+
+var MCQS4 = prompt(`What does CSS stand for______________.
+    
+a) Cascading Style Sheets
+b) Creative Style Syntax
+c) Computer Style System
+d) Colorful Styling Sheets`
+)
+
+
+if (MCQS4 == `a`) {
+    alert(`your answer is correct ✅`)
+     document.writeln
+(`4) Correct`+ `<br>`)
+} else {
+    alert(`your answer is incorrect ❌`)
+     document.writeln
+(`4) Incorrect`+ `<br>`)
+}
+
+
+var MCQS5 = prompt(`Which CSS property is used to change the text color of an element______________.
+    
+a) background-color
+b) font-color
+c) text-color
+d) color`
+)
+
+
+if (MCQS5 == `d`) {
+    alert(`your answer is correct ✅`)
+     document.writeln
+(`5) Correct`+ `<br>`)
+
+} else {
+    alert(`your answer is incorrect ❌`)
+     document.writeln
+(`5) Incorrect`+ `<br>`)
+}
+
+
+var MCQS6 = prompt(`Which CSS property is used to specify the font family for an element______________.
+    
+a) font-style
+b) text-font
+c) font-family
+d) typeface`
+
+)
+
+
+if (MCQS6 == `c`) {
+    alert(`your answer is correct ✅`)
+     document.writeln
+(`6) Correct` + `<br>`)
+
+} else {
+    alert(`your answer is incorrect ❌`)
+     document.writeln
+(`6) Incorrect`+ `<br>`)
+}
+
+
+
+var MCQS6 = prompt(`How do you select an element with the ID "header" in CSS______________.
+    
+a) .header
+b) #header
+c) element#header
+d) header`
+)
+
+
+if (MCQS6 == `b`) {
+    alert(`your answer is correct ✅`
+
+    )
+     document.writeln
+(`7) Correct` + `<br>`)
+
+} else {
+    alert(`your answer is incorrect ❌`)
+     document.writeln
+(`8) Incorrect` + `<br>` )
+}
+
+
+
+var MCQS6 = prompt(`Which of the following is the correct way to include an external CSS file in an HTML document______________.
+    
+a) <style src="style.css"></style>
+b) <css src="style.css">
+c) <link rel="stylesheet" href="style.css">
+d) <import style="style.css">`
+
+)
+
+
+if (MCQS6 == `c`) {
+    alert(`your answer is correct ✅`)
+     document.writeln
+(`7) Correct`+ `<br>`)
+
+} else {
+    alert(`your answer is incorrect ❌`)
+     document.writeln
+(`7) Incorrect`+ `<br>`)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
